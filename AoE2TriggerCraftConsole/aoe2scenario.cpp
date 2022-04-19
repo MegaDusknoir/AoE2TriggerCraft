6ffb1dba2561acd08125b1db8e9b02d472dfb522
MegaDusknoir <59289335+MegaDusknoir@users.noreply.github.com>
2022-04-19 11:31:03 +0800

    change construct

--- AoE2TriggerCraftConsole/aoe2scenario.cpp
+++ AoE2TriggerCraftConsole/aoe2scenario.cpp
@@ -4,14 +4,23 @@
 namespace AoE2ScenarioNamespace
 {
     constexpr char current_version[] = "1.41";
-    void AoE2Scenario::open_file(ifstream& fin)
+    AoE2Scenario::AoE2Scenario(const char* file_path)
     {
-        stringstream ssin;
-        ssin << fin.rdbuf();
-        string raw_binary(ssin.str());
-        auto check_version = raw_binary.substr(0, 4);
+        ifstream fin(file_path, ios::binary);
+        if (fin.fail() == true)
+        {
+            throw;
+        }
+        string check_version(4, 0);
+        fin.read(reinterpret_cast<char *>(&check_version[0]), 4);
         if (check_version == string(current_version))
         {
+            fin.seekg(0, ios::end);
+            auto file_size = static_cast<size_t>(fin.tellg());
+            fin.seekg(ios::beg);
+            string raw_binary(file_size, 0);
+            fin.read(reinterpret_cast<char *>(&raw_binary[0]), file_size);
+            fin.close();
 #ifndef NDEBUG
             auto t1 = clock();
 #endif
@@ -26,9 +35,11 @@ namespace AoE2ScenarioNamespace
         }
         else if (check_version == string("1.41"))
         {
+            fin.close();
         }
         else
         {
+            fin.close();
 #ifndef NDEBUG
             std::cout << "Version " << check_version << " not supported yet." << std::endl;
 #endif
