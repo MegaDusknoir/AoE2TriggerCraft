b556eca54b6e320edbbca4389fcda1a40571edf0
Dusknior <dusknior@gmail.com>
2022-04-23 02:25:13 +0800

    create 3 windows to divide area; create property sheet; add state bar

--- AoE2TCWin/AoE2TCWin.cpp
+++ AoE2TCWin/AoE2TCWin.cpp
@@ -1,5 +1,9 @@
 ﻿// AoE2TCWin.cpp : 定义应用程序的入口点。
 //
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment (lib, "comctl32")
 
 #include <map>
 #include "framework.h"
@@ -16,6 +20,13 @@ WCHAR szScenName[MAX_LOADSTRING];
 HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+HWND hBar;
+WCHAR szEditorWindowClass[MAX_LOADSTRING] = L"EditorWindowClass";
+HWND hEditor;
+WCHAR szViewerWindowClass[MAX_LOADSTRING] = L"ViewerWindowClass";
+HWND hViewer;
+WCHAR szInfoWindowClass[MAX_LOADSTRING] = L"InfoWindowClass";
+HWND hInfo;
 
 class StringMap
 {
@@ -46,9 +57,293 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
 void OpenFile(HWND hWnd);
 void SaveFile(HWND hWnd);
+
+LRESULT CALLBACK EditorWndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT ret = 0;
+    switch (msg)
+    {
+    default:
+        ret = DefWindowProc(window, msg, wParam, lParam);
+    }
+
+    return ret;
+}
+ATOM MakeEditorClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = EditorWndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = NULL;
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szEditorWindowClass;
+    wcex.hIconSm = NULL;
+
+    return RegisterClassExW(&wcex);
+}
+HWND CreateEditorWnd(HWND hParent, int x)
+{
+    static bool registered = false;
+
+    // WTF is this for? Yeah, I've read the docs, but the question remains.
+    HINSTANCE instance = GetModuleHandle(NULL);
+
+    if (!registered)
+    {
+        MakeEditorClass(instance);
+        registered = true;
+    }
+
+    RECT rect;
+    GetClientRect(hParent, &rect);
+    HWND hEditor = CreateWindowEx(0, szEditorWindowClass, L"编辑器区",
+        WS_OVERLAPPED | WS_BORDER | WS_CHILD | WS_VISIBLE,
+        0, 0, x, rect.bottom - rect.top,
+        hParent, nullptr, instance, nullptr);
+
+    if (!hEditor)
+    {
+        return FALSE;
+    }
+
+    return hEditor;
+}
+
+
+LRESULT CALLBACK InfoWndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT ret = 0;
+    switch (msg)
+    {
+    default:
+        ret = DefWindowProc(window, msg, wParam, lParam);
+    }
+
+    return ret;
+}
+ATOM MakeInfoClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = InfoWndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = NULL;
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szInfoWindowClass;
+    wcex.hIconSm = NULL;
+
+    return RegisterClassExW(&wcex);
+}
+HWND CreateInfoWnd(HWND hParent, int y)
+{
+    static bool registered = false;
+
+    // WTF is this for? Yeah, I've read the docs, but the question remains.
+    HINSTANCE instance = GetModuleHandle(NULL);
+
+    if (!registered)
+    {
+        MakeInfoClass(instance);
+        registered = true;
+    }
+
+    RECT rect;
+    GetClientRect(hParent, &rect);
+    RECT rectEditor;
+    GetClientRect(hEditor, &rectEditor);
+    HWND hInfo = CreateWindowEx(0, szInfoWindowClass, L"详细信息区",
+        WS_OVERLAPPED | WS_BORDER | WS_CHILD | WS_VISIBLE,
+        rectEditor.right + 2, y, rect.right - (rectEditor.right + 2), rect.bottom - y,
+        hParent, nullptr, instance, nullptr);
+
+    if (!hInfo)
+    {
+        return FALSE;
+    }
+
+    return hInfo;
+}
+
+LRESULT CALLBACK ViewerWndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT ret = 0;
+    switch (msg)
+    {
+    default:
+        ret = DefWindowProc(window, msg, wParam, lParam);
+    }
+
+    return ret;
+}
+ATOM MakeViewerClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = ViewerWndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = NULL;
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szViewerWindowClass;
+    wcex.hIconSm = NULL;
+
+    return RegisterClassExW(&wcex);
+}
+HWND CreateViewerWnd(HWND hParent)
+{
+    static bool registered = false;
+
+    // WTF is this for? Yeah, I've read the docs, but the question remains.
+    HINSTANCE instance = GetModuleHandle(NULL);
+
+    if (!registered)
+    {
+        MakeViewerClass(instance);
+        registered = true;
+    }
+
+    RECT rect;
+    RECT rectEditor;
+    RECT rectInfo;
+    GetClientRect(hParent, &rect);
+    GetClientRect(hEditor, &rectEditor);
+    GetClientRect(hInfo, &rectInfo);
+    HWND hViewer = CreateWindowEx(0, szViewerWindowClass, L"详细信息区",
+        WS_OVERLAPPED | WS_BORDER | WS_CHILD | WS_VISIBLE,
+        rectEditor.right + 2, 0, rect.right - (rectEditor.right + 2), rect.bottom - (rectInfo.bottom + 2),
+        hParent, nullptr, instance, nullptr);
+
+    if (!hViewer)
+    {
+        return FALSE;
+    }
+
+    return hViewer;
+}
+
+
+#define EDITOR_SHEET_NUM_PAGES 2
+INT_PTR CALLBACK TrigDlgProc(HWND dialog, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+        //TODO
+        break;
+    }
+
+    return 0;
+}
+
+DLGPROC procs[EDITOR_SHEET_NUM_PAGES] =
+{
+    &TrigDlgProc,
+    &TrigDlgProc,
+};
+const WORD PropSheetButtons[] =
+{ IDOK, IDCANCEL, IDHELP };
+
+int CALLBACK PropSheetProc(HWND sheet, UINT uMsg, LPARAM lParam)
+{
+    switch (uMsg)
+	{
+	case PSCB_PRECREATE:
+	{
+        RECT rect;
+        GetClientRect(hEditor, &rect);
+		DLGTEMPLATE* templ = (DLGTEMPLATE*)lParam;
+		//add a minimize box
+		templ->style |= WS_OVERLAPPED | WS_CHILD | WS_VISIBLE | WS_MAXIMIZE;
+        templ->style &= ~(WS_POPUP | WS_BORDER | DS_CENTER | DS_MODALFRAME | DS_3DLOOK | DS_ABSALIGN | WS_CAPTION | WS_DLGFRAME | WS_SYSMENU);
+        templ->x = (short)rect.left;
+        templ->y = (short)rect.top;
+        templ->cx = (short)rect.right;
+        templ->cy = (short)rect.bottom;
+	}
+        break;
+    case PSCB_INITIALIZED:
+    {
+        /* Remove unused buttons. */
+        for (int i = 0; i < sizeof(PropSheetButtons) / sizeof(WORD); i++)
+        {
+            HWND hWnd = GetDlgItem(sheet, PropSheetButtons[i]);
+            if (hWnd != NULL)
+            {
+                ShowWindow(hWnd, SW_HIDE);
+                EnableWindow(hWnd, FALSE);
+            }
+        }
+    }
+	    break;
+    default:
+        return 0;
+    }
+    return 0;
+}
+
+HWND MakeEditorSheet(HINSTANCE app)
+{
+    PROPSHEETHEADER header;
+    HPROPSHEETPAGE pages[EDITOR_SHEET_NUM_PAGES];
+    PROPSHEETPAGE pg;	//used to create each page
+    HWND sheet;
+
+    //create pages
+
+    pg.dwSize = sizeof(PROPSHEETPAGE);
+    pg.dwFlags = PSP_DEFAULT | PSP_HIDEHEADER;
+    pg.hInstance = app;
+
+    for (int i = 0; i < EDITOR_SHEET_NUM_PAGES; i++)
+    {
+        pg.pszTemplate = MAKEINTRESOURCE(IDD_TRIGGERS + i);	//template IDs are in display order
+        pg.pfnDlgProc = procs[i];
+        pg.lParam = 0;
+        pages[i] = CreatePropertySheetPage(&pg);
+    }
+
+    //create sheet
+
+    header.dwSize = sizeof(header);
+    header.dwFlags = PSH_MODELESS | PSH_USECALLBACK | PSH_NOMARGIN |
+        PSH_NOAPPLYNOW | PSH_NOCONTEXTHELP;
+    header.hwndParent = hEditor;
+    header.hInstance = app;
+    header.pszCaption = szEditorWindowClass;
+    header.nPages = EDITOR_SHEET_NUM_PAGES;
+    header.nStartPage = 0;
+    header.phpage = pages;
+
+    header.pfnCallback = &PropSheetProc;
+
+    sheet = (HWND)PropertySheet(&header);
+
+    return sheet;
+}
+
 const wchar_t* getFilenameFromPath(const wchar_t* path)
 {
 	const wchar_t* ret;
@@ -91,6 +386,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     LoadStringMap(hInstance, IDS_SAVE_SUCCESS, stMap, MAX_LOADSTRING);
     LoadStringMap(hInstance, IDS_SAVE_FAIL, stMap, MAX_LOADSTRING);
     LoadStringMap(hInstance, IDS_OPEN_FAIL, stMap, MAX_LOADSTRING);
+    LoadStringMap(hInstance, IDS_WELCOME, stMap, MAX_LOADSTRING);
+    LoadStringMap(hInstance, IDS_OPEN_SUCCESS, stMap, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // 执行应用程序初始化:
@@ -158,20 +455,81 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 将实例句柄存储在全局变量中
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   HWND hWnd = CreateWindowEx(
+       WS_EX_ACCEPTFILES,                              // Optional window styles.
+	   szWindowClass,                     // Window class
+	   szTitle,    // Window text
+	   WS_CLIPCHILDREN | WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_THICKFRAME),            // Window style
+
+	   // Size and position
+	   CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+	   nullptr,       // Parent window    
+	   nullptr,       // Menu
+	   hInstance,  // Instance handle
+	   nullptr        // Additional application data
+   );
 
    if (!hWnd)
    {
-      return FALSE;
+	   return FALSE;
    }
 
+   hEditor = CreateEditorWnd(hWnd, 300);
+   if (!hEditor)
+   {
+	   return FALSE;
+   }
+   hInfo = CreateInfoWnd(hWnd, 500);
+   if (!hInfo)
+   {
+       return FALSE;
+   }
+   hViewer = CreateViewerWnd(hWnd);
+   if (!hViewer)
+   {
+       return FALSE;
+   }
+   MakeEditorSheet(hInstance);
+   hBar = CreateWindow(STATUSCLASSNAME, stMap[IDS_WELCOME],
+	   WS_CHILD | WS_VISIBLE, 0, 0, 0, 0,
+       hWnd, NULL, hInstance, NULL);
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
    return TRUE;
 }
-
+void OnSize(HWND hWnd)
+{
+    RECT rect;
+    RECT rectEditor;
+    RECT rectInfo;
+    RECT rectViewer;
+    GetClientRect(hWnd, &rect);
+    GetClientRect(hEditor, &rectEditor);
+    GetClientRect(hInfo, &rectInfo);
+    GetClientRect(hViewer, &rectViewer);
+    LONG Info_height = rectInfo.bottom - rectInfo.top + 2;
+    LONG Editor_width = rectEditor.right - rectEditor.left + 2;
+    MoveWindow(hEditor,
+        0,
+        0,
+        Editor_width,
+        rect.bottom - rect.top,
+        TRUE);
+    MoveWindow(hInfo,
+        rectEditor.right + 2,
+        rect.bottom - Info_height,
+        rect.right - (rectEditor.right + 2),
+        Info_height,
+        TRUE);
+    MoveWindow(hViewer,
+        rectEditor.right + 2,
+        0,
+        rect.right - (rectEditor.right + 2),
+        rect.bottom - (rectInfo.bottom + 2),
+        TRUE);
+}
 //
 //  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -212,16 +570,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
+    case WM_WINDOWPOSCHANGED:
+    case WM_SIZING:
+        {
+		    //RECT* rect = (RECT*)lParam;
+            OnSize(hWnd);
+        }
+        break;
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
             EndPaint(hWnd, &ps);
         }
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
+        break;
+    case WM_DROPFILES:
+        ;
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
@@ -289,7 +658,7 @@ void OpenFile(HWND hWnd)
                             try
                             {
                                 Scen.open(pszFilePath);
-                                MessageBox(NULL, pszFilePath, stMap[IDS_OPEN], MB_OK);
+                                //MessageBox(NULL, pszFilePath, stMap[IDS_OPEN], MB_OK);
                                 // set status bar text
                                 wchar_t titleBuffer[100];
                                 auto filename = getFilenameFromPath(pszFilePath);
@@ -297,10 +666,12 @@ void OpenFile(HWND hWnd)
 								wnsprintf(titleBuffer, sizeof(titleBuffer) / sizeof(wchar_t),
 									L"%s - %s", szTitle, szScenName);
                                 SetWindowText(hWnd, titleBuffer);
+                                SetWindowText(hBar, stMap[IDS_OPEN_SUCCESS]);
                             }
                             catch (std::exception& ex)
                             {
                                 MessageBox(NULL, stMap[IDS_OPEN_FAIL], stMap[IDS_OPEN], MB_ICONWARNING);
+                                SetWindowText(hBar, stMap[IDS_OPEN_FAIL]);
                             }
                             CoTaskMemFree(pszFilePath);
                         }
--- AoE2TCWin/resource.h
+++ AoE2TCWin/resource.h
@@ -17,8 +17,17 @@
 #define IDS_SAVE_FAIL                   108
 #define IDC_AOE2TCWIN                   109
 #define IDS_OPEN_FAIL                   110
+#define IDS_EDITOR_TITLE_TRIGGER        111
+#define IDS_EDITOR_TITLE_TERRAIN        112
+#define IDS_EDITOR_TITLE_UNITS          113
+#define IDS_WELCOME                     114
+#define IDS_OPEN_SUCCESS                115
 #define IDR_MAINFRAME                   128
 #define IDS_ALL_TYPE                    129
+#define IDD_TRIGGERS                    131
+#define IDD_UNITS                       132
+#define IDC_TREE1                       1007
+#define IDC_TRIGGER_TREE                1007
 #define ID_32771                        32771
 #define IDM_OPEN                        32772
 #define ID_32773                        32773
@@ -30,9 +39,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
